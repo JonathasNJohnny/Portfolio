0e2d08ae1c27ab6474cc55b86ae42b6192eaa3de
JonathasNJohnny <jonathasnobrega59@gmail.com>
2024-04-12 15:10:51 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import './App.css';
 import RoutesApp from './routes/Routes.jsx'
-import Navbar from './components/Navbar/Navbar.jsx'
-import Banner from './components/Banner/Banner.jsx'
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Banner/>
       <RoutesApp/>
     </div>
   )
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,11 +1,13 @@
 import styles from "./Homepage.module.css"
+import Navbar from '../../components/Navbar/Navbar.jsx'
+import Banner from '../../components/Banner/Banner.jsx'
 
 export const Homepage = () => {
-    return (
-        <>
-            <div className={styles.homepageContent}>
-            </div>
-        </>
+    return (  
+        <div className={styles.homepageContent}>
+            <Navbar/>
+            <Banner/>
+        </div>
         
     )
 }
