f90b8e1c37dc65e14652f48b078bc55c89585766
JonathasNJohnny <jonathasnobrega59@gmail.com>
2024-04-13 17:42:35 -0300

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,25 @@
 import Button1 from '../Button1/Button1';
 import styles from './Navbar.module.css';
 import { Icon, InlineIcon } from "@iconify/react";
+import linkedInIcon from '../../assets/linkedInIcon.png'
+import githubIcon from '../../assets/githubIcon.png'
 
 export const Navbar = () => {
     return (
         <>
             <div className={styles.fixedNavbar}>
                 <div className={styles.navbar}>
-                    <h2>JN</h2>
+                    <h2 className={styles.gradient}>JN</h2>
                     <div className={styles.navLinks}>
                         <ul className={styles.linksContainer}>
-                            <li>Skills</li>
-                            <li>Projects</li>
+                            <li className={styles.gradient}>Skills</li>
+                            <li className={styles.gradient}>Projects</li>
                         </ul>
                     </div>
                     <div className={styles.contact}>
                         <ul>
-                            <li className={styles.iconli}><Icon icon='entypo-social:linkedin-with-circle' style={{color: '#adadad'}} width={45}/></li>
-                            <li className={styles.iconli}><Icon icon='entypo-social:github-with-circle' style={{color: '#adadad'}} width={45}/></li>
+                            <li className={styles.iconli}><img src={linkedInIcon} alt='LinkedInIcon' width={45} className={styles.linkedInIcon}/></li>
+                            <li className={styles.iconli}><img src={githubIcon} alt='GithubIcon' width={45} className={styles.githubIcon}/></li>
                             <li className={styles.buttonli}><Button1 buttonText={"Let's Connect!"}/></li>
                         </ul>
                     </div>
