52e1d0e223bfd89498cc98ff41a6eb0a590f3bb8
Jonathas Nóbrega <57995489+JonathasNJohnny@users.noreply.github.com>
2024-04-15 15:37:57 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'react-router-dom': 'react-router-dom/dist/react-router-dom.esm.js'
-    }
-  },
   build: {
     rollupOptions: {
-      external: ['react-router-dom']
+      external: /^react-router-dom($|\/)/
     }
   }
 });
