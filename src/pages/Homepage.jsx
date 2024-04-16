f4e15bdb23eff906ad94a29bdf1af05d9abf4996
Jonathas Nóbrega <57995489+JonathasNJohnny@users.noreply.github.com>
2024-04-16 01:05:11 -0300

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,6 @@
-import styles from "./Homepage.module.css"
-import Navbar from '../../components/Navbar/Navbar.jsx'
-import Banner from '../../components/Banner/Banner.jsx'
+import styles from './Homepage.module.css'
+import Navbar from '../components/Navbar/Navbar.jsx'
+import Banner from '../components/Banner/Banner.jsx'
 
 export const Homepage = () => {
     return (  
