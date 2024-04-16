7b5fc31236121760fa4fb00ec195f1dc74077e4f
Jonathas Nóbrega <57995489+JonathasNJohnny@users.noreply.github.com>
2024-04-16 01:03:58 -0300

    Update Routes.jsx

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Homepage from '../pages/homepage/Homepage'
+import Homepage from '../pages/Homepage'
 
 function RoutesApp() {
     return (
